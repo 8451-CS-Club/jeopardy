72e08a3966ee692fd0be4a8eda2be6c22de6270b
Ethan Swan <eswan18@rocketmail.com>
2016-12-21 20:42:18 -0500

    Solidified framework for POST requests

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from app import app
 
 
@@ -12,8 +12,23 @@ def game():
 
 @app.route('/get_categories/', methods = ['POST'])
 def get_categories():
+	try:
+		n_cat = request.form['n_cat']
+	except:
+		return "ERROR: Bad get_categories request"
+	#This needs to be implemented:
+	#return $DAVIS_API_get_categories(n_cat)
 	return "categories_here"
 
 @app.route('/get_questions_in_cat/', methods = ['POST'])
 def get_questions_in_cat():
-	return "questions_here"
+	try:
+		category = request.form['category']
+		n_q = request.form['n_q']
+	except:
+		return "ERROR: Bad get_questions_in_cat request"
+	#This needs to be implemented:
+	#data = $DAVIS_API_get_questions_in_cat(category, n_q)
+	#TEMPORARY - Fake data returned
+	data = {'q':'a', 'question':'answer'}
+	return jsonify(**data)
