dc19400ff65fec7d712b10d901f5591330a765f3
Ethan Swan <eswan18@rocketmail.com>
2016-12-21 20:09:01 -0500

    Added skeleton of POST requests

--- app/views.py
+++ app/views.py
@@ -5,3 +5,11 @@ from app import app
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/get_categories/', methods = ['POST'])
+def get_categories():
+	return "categories here"
+
+@app.route('/get_questions_in_cat/', methods = ['POST'])
+def get_questions_in_cat():
+	return "questions_here"
